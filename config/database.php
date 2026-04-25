0c75155b950e6a9da06db6009f2e1a32b97028da
muhamaddikrulloh <muhamaddikrulloh@gmail.com>
2026-04-25 21:02:40 +0700

    Add Composer configuration and update database connection to use dotenv

--- config/database.php
+++ config/database.php
@@ -1,8 +1,13 @@
 <?php
-$host = getenv('DB_HOST');
-$db   = getenv('DB_NAME');
-$user = getenv('DB_USER');
-$pass = getenv('DB_PASS');
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+$host = $_ENV['DB_HOST'];
+$db   = $_ENV['DB_NAME'];
+$user = $_ENV['DB_USER'];
+$pass = $_ENV['DB_PASS'];
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $user, $pass);
