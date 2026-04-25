ec7be255eead2a82effc2e30a096532071057246
muhamaddikrulloh <muhamaddikrulloh@gmail.com>
2026-04-25 20:42:26 +0700

    add .env file for database configuration and update database.php to use environment variables

--- config/database.php
+++ config/database.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'localhost';
-$db   = 'bandung-profile';
-$user = 'root';
-$pass = '';
+$host = getenv('DB_HOST');
+$db   = getenv('DB_NAME');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASS');
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $user, $pass);
